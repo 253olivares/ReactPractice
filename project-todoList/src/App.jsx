d1f0f4e1d60eccbe5e7e884ee99e0abc889b7cf8
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-01-22 20:03:34 -0500

    Adding last bit of comments

--- project-todoList/src/App.jsx
+++ project-todoList/src/App.jsx
@@ -1,10 +1,11 @@
 import ToDoList from "./components/ToDoList"
-
+// import our component
 function App() {
   
 
   return (
     <>
+    {/* Todo component */}
       <ToDoList></ToDoList>
     </>
   )
--- project-todoList/src/components/ToDoList.jsx
+++ project-todoList/src/components/ToDoList.jsx
@@ -1,26 +1,39 @@
 import React, {useState} from 'react'
+// Import React and use State 
 
 function ToDoList() {
-    const [tasks,setTasks] = useState(["Eat Breakfast","Take a shower","Walk the dog"]);
-    const [newTask, setNewTask] = useState("");
+    // todo list component
 
+    // create task state this will keep track of our task array. Every time we make a change we will be mutating this array and having react rerender our dom
+    // to reflect the change
+    const [tasks,setTasks] = useState(["Eat Breakfast","Take a shower","Walk the dog"]);
+    // New task will be our task within our input
+    const [newTask, setNewTask] = useState("");
+    // function to change our new task and set the input value 
     function handleInputChange(event) {
         setNewTask(event.target.value);
     }
-
+    // function that if our new task is not an empty string adds to our existing array at the very start
     function addTask() {
-
+// Trim gets rid of any blank space
         if(newTask.trim()!== ""){
+            // set tasks adds our new task to our existing array by spreading out our current tasks and appending the new one to the end
             setTasks((prevTasks)=>[...prevTasks, newTask]);
+            // Clear our new task so our input is blank
             setNewTask("");
         }
     }
-
+    // Delete tasks takes the index of the item in the array when it was renders and filters it out of the array with the i === index
     function deleteTask(index) {
         const updatedTasks = tasks.filter((_,i)=> i!==index);
         setTasks(updatedTasks);
     }
-
+    //  Move task up checks to make sure our index is greater than 0 if so we create a new const called task. We spread our current
+    // task array creating a duplicate.
+    // that we deconstruct the duplicated array 
+    // Our task becomes the task the was one index below ours and the index that was below us becomes what our previous task index was.
+    // This new task array is then set to replace our previous array
+    // Page then updates and rerenders our array showing the change
     function moveTaskUp(index) {
         if(index > 0){
             const updatedTasks = [...tasks];
@@ -28,7 +41,7 @@ function ToDoList() {
             setTasks(updatedTasks)
         }
     }
-
+    // Same as moveTaskUp but in reverse
     function moveTaskDown(index) {
         if(index < tasks.length-1){
             const updatedTasks = [...tasks];
@@ -38,30 +51,37 @@ function ToDoList() {
     }
 
     return (
-        <>
+        <React.Fragment>
+            {/* our Todo List */}
         <div className='to-do-list'>
             <h1>
                 To-Do-List
             </h1>
-
+            {/* This div is where we will be be using to create a new task
+            we bind our task state to the value and create an onchange listener to run handle Input change every time our dom detects
+            input*/}
             <div>
                 <input 
                 type="text"
                 placeholder='Enter a task...'
                 value={newTask}
                 onChange={handleInputChange} />
+                {/* Button Adds task to our array */}
                 <button
                 className='add-button'
                 onClick={addTask}>
                     Add
                 </button>
             </div>
-
+            {/* Order list that maps our task when it detects a change in the state and rerenders new list with any changes to teh array.
+            Alongside the new array we create an index that we use to keep track or todo listings */}
             <ol>
                 {tasks.map((task,index)=> 
                 <li
                 key={index}
                 >
+                    {/* every rerender we add some additional on click listeners and buttons that keep track of the todos index and runs functions that
+                    either deletes our todo moves it up or down */}
                     <span className='task'>{task}</span>
                     <button
                     className='delete-button'
@@ -81,7 +101,7 @@ function ToDoList() {
                 </li>)}
             </ol>
         </div>
-        </>
+        </React.Fragment>
     )
 }
 export default ToDoList
